50d351c900698cf1d26e123c1294dac62896d3f5
TGVGOD <87058188+TGVG0D@users.noreply.github.com>
2026-07-10 22:34:48 -0300

    Update settings.php

--- settings.php
+++ settings.php
@@ -8,7 +8,7 @@ $messageType = '';
 $envFile = __DIR__ . '/.env';
 $env = readEnv($envFile);
 
-// Detecta se está usando hash ou texto puro s
+// Detecta se está usando hash ou texto puro 
 $currentUsername = '';
 if (isset($env['APP_USER_HASH'])) {
     $currentUsername = '(criptografado)';
